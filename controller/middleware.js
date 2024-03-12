db3785059dd7716405e0d89ebb7c8bdc34be924d
rohitpatils7913 <133487615+rohitpatils7913@users.noreply.github.com>
2024-03-12 23:34:30 +0530

    Create middleware.js

---
+++ controller/middleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+
+module.exports = auth;
