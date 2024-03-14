c21a83e65f0824de3c8055d85b62050508bf176d
rohitpatils7913 <133487615+rohitpatils7913@users.noreply.github.com>
2024-03-14 21:45:22 +0530

    Update fileuploadModel.js

--- models/fileuploadModel.js
+++ models/fileuploadModel.js
@@ -1,1 +1,26 @@
 
+const mongoose = require('mongoose');
+
+const fileuploadSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Phonenumber: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const fileupload = mongoose.model('fileupload', fileuploadSchema);
+
+module.exports = fileupload;
