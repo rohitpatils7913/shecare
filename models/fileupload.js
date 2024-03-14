04721579e560aa601a18b6753d0fb7a65a300677
rohitpatils7913 <133487615+rohitpatils7913@users.noreply.github.com>
2024-03-14 23:19:05 +0530

    Update fileupload.js

--- models/fileupload.js
+++ models/fileupload.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const fileSchema = new mongoose.Schema({
     filename: String,
     path: String,
+    user_id:String,
     timestamp: { type: Date, default: Date.now }
 });
 
