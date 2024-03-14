2bcb47f1cacc6340ffcd4dcee785ccaad96939bc
rohitpatils7913 <133487615+rohitpatils7913@users.noreply.github.com>
2024-03-14 21:47:31 +0530

    Update registration.js

--- controller/registration.js
+++ controller/registration.js
@@ -7,6 +7,7 @@ const Contact = require('../models/contact');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fileupload= require("../model/fileupload")
 
 const emergencyRegister = async (req, res) => {
   try {
